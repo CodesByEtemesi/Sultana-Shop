f0acc75ccc3f924c93f6ccd18559d7e2b6f84209
ETEMESI <samweletemesi2@Gmail.com>
2024-08-28 10:28:39 +0300

    Created the List.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,12 +7,14 @@ import Contact from './pages/Contact'
 import Product from './pages/Product'
 import Cart from './pages/Cart'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     // Important routes
     <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
 
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/collection' element={<Collection />} />
